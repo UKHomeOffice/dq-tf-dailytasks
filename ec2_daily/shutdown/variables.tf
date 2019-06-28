388e43819ae0919d56685a68a5d1e8a22a5b73ce
Olaleye Alex-Oni <olaleyealex-oni@dqdevlap994.home>
2019-06-28 08:07:09 +0100

    shutdown config added

---
+++ ec2_daily/shutdown/variables.tf
@@ -0,0 +1,10 @@
+variable "naming_suffix" {
+   default = "notprod"
+}
+
+variable "path_module" {
+   default = "unset"
+}
+
+locals {
+   path_module =  "${var.path_module != "unset" ? var.path_module : path.module}"
