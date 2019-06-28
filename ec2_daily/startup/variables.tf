c69f24bf9bd70e6c50df6679136a137932b06583
Olaleye Alex-Oni <olaleyealex-oni@dqdevlap994.home>
2019-06-28 07:46:25 +0100

    directory structure modified

---
+++ ec2_daily/startup/variables.tf
@@ -0,0 +1,11 @@
+variable "naming_suffix" {
+  default = "notprod"
+}
+
+variable "path_module" {
+  default = "unset"
+}
+
+locals {
+  path_module = "${var.path_module != "unset" ? var.path_module : path.module}"
+}
