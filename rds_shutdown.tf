9bf8be4dd7e91e81f30fb300900efbf18fc34f3c
Olaleye Alex-Oni <olaleyealex-oni@DQDEVLAP994.local>
2019-07-04 11:00:43 +0100

    modified data archive directory value

--- rds_shutdown.tf
+++ rds_shutdown.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 data "archive_file" "rds_shutdownzip" {
   type   =  "zip"
-  source_file = "${local.path_module}/lambda/code/rds_shutdown.py"
+  source_dir = "${local.path_module}/lambda/code"
   output_path = "${local.path_module}/lambda/package/rds_shutdownzip"
 }
 
