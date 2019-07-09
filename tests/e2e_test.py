d65ccb7f3ad378819605f24fafbc516213147c24
Olaleye Alex-Oni <olaleyealex-oni@DQDEVLAP994.local>
2019-07-09 16:01:21 +0100

    module values updated - dailytask

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -15,7 +15,7 @@ class TestE2E(unittest.TestCase):
               skip_get_ec2_platforms = true
             }
 
-            module "dailytasks" {
+            module "root_modules" {
               source = "./mymodule"
               #providers = {aws = "aws"}
 
@@ -30,28 +30,28 @@ class TestE2E(unittest.TestCase):
         self.assertEqual(self.result["destroy"], False)
 
     def test_name_suffix_aws_lambda_function_rdsshutdown_tags(self):
-        self.assertEqual(self.result['dailytasks']["aws_lambda_function.rds-shutdown-function"]["tags.Name"], "rds_shutdown-apps-preprod-dq")
+        self.assertEqual(self.result['root_modules']["aws_lambda_function.rds-shutdown-function"]["tags.Name"], "rds_shutdown-apps-preprod-dq")
 
     def test_name_suffix_aws_iam_role_rds_shutdown_role_tags(self):
-          self.assertEqual(self.result['dailytasks']["aws_iam_role.rds-shutdown_role"]["tags.Name"], "rds-shutdown_role-apps-preprod-dq")
+          self.assertEqual(self.result['root_modules']["aws_iam_role.rds-shutdown_role"]["tags.Name"], "rds-shutdown_role-apps-preprod-dq")
 
     def test_name_suffix_aws_lambda_function_rdsstartup_tags(self):
-          self.assertEqual(self.result['dailytasks']["aws_lambda_function.rds_startup-function"]["tags.Name"], "rds_daily_startup-apps-preprod-dq")
+          self.assertEqual(self.result['root_modules']["aws_lambda_function.rds_startup-function"]["tags.Name"], "rds_daily_startup-apps-preprod-dq")
 
     def test_name_suffix_aws_iam_role_rds_startup_role_tags(self):
-          self.assertEqual(self.result['dailytasks']["aws_iam_role.rds_startup_role"]["tags.Name"], "rds_startup_role-apps-preprod-dq")
+          self.assertEqual(self.result['root_modules']["aws_iam_role.rds_startup_role"]["tags.Name"], "rds_startup_role-apps-preprod-dq")
 
     def test_name_suffix_aws_lambda_function_ec2startup_tags(self):
-        self.assertEqual(self.result['dailytasks']["aws_lambda_function.ec2-startup-function"]["tags.Name"], "ec2_daily_startup-apps-preprod-dq")
+        self.assertEqual(self.result['root_modules']["aws_lambda_function.ec2-startup-function"]["tags.Name"], "ec2_daily_startup-apps-preprod-dq")
 
     def test_name_suffix_aws_iam_role_ec2_startup_role_tags(self):
-          self.assertEqual(self.result['dailytasks']["aws_iam_role.ec2_startup_role"]["tags.Name"], "ec2_startup_role-apps-preprod-dq")
+          self.assertEqual(self.result['root_modules']["aws_iam_role.ec2_startup_role"]["tags.Name"], "ec2_startup_role-apps-preprod-dq")
 
     def test_name_suffix_aws_lambda_function_ec2shutdown_tags(self):
-          self.assertEqual(self.result['dailytasks']["aws_lambda_function.ec2-shutdown-function"]["tags.Name"], "ec2_daily_shutdown-apps-preprod-dq")
+          self.assertEqual(self.result['root_modules']["aws_lambda_function.ec2-shutdown-function"]["tags.Name"], "ec2_daily_shutdown-apps-preprod-dq")
 
     def test_name_suffix_aws_iam_role_ec2_shutdown_role_tags(self):
-          self.assertEqual(self.result['dailytasks']["aws_iam_role.ec2_shutdown_role"]["tags.Name"], "ec2_shutdown_role-apps-preprod-dq")
+          self.assertEqual(self.result['root_modules']["aws_iam_role.ec2_shutdown_role"]["tags.Name"], "ec2_shutdown_role-apps-preprod-dq")
 
 if __name__ == '__main__':
     unittest.main()
