9859d917da6a3a94befd90efccbdb03c387ebc25
Olaleye Alex-Oni <olaleyealex-oni@dqdevlap994.home>
2019-07-10 15:20:10 +0100

    variables changes redacted

--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 locals {
     naming_suffix = "${var.naming_suffix}"
+    path_module   = "${var.path_module != "unset" ? var.path_module : path.module}"
 }
 
-#variable "path_module" {
-#   default = "unset"
-#}
+variable "path_module" {
+   default = "unset"
+}
 
 variable "namespace" {
    default = "notprod"
