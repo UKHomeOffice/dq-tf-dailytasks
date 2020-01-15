f994734a988ff1f7d533c578459febc66e0e3e0f
Jwanderema <wanderema@hotmail.co.uk>
2020-01-15 12:30:24 +0000

    removing unecessary subnets and security group

--- variables.tf
+++ variables.tf
@@ -22,15 +22,3 @@ variable "pipeline_name" {
 variable "pipeline_count" {
   default = 1
 }
-
-variable "lambda_subnet" {
-  default = "subnet-05f088f2a4a2fd968"
-}
-
-variable "lambda_subnet_az2" {
-  default = "subnet-04e1ded8159dbc3ee"
-}
-
-variable "lambda_sgrp" {
-  default = "sg-08a996ab577bdb8aa"
-}
