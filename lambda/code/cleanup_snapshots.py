a89988114826330abb8ce4c8fc08032de6e62641
Jwanderema <wanderema@hotmail.co.uk>
2020-01-15 11:20:47 +0000

    relint py code

--- lambda/code/cleanup_snapshots.py
+++ lambda/code/cleanup_snapshots.py
@@ -6,10 +6,11 @@
 """
 
 import sys
+import re
 import logging
-import boto3
-import botocore
+import json
 import urllib.request
+import boto3
 from botocore.config import Config
 from botocore.exceptions import ClientError
 
@@ -23,6 +24,27 @@ CONFIG = Config(
         max_attempts=20
     )
 )
+
+def error_handler(lineno, error):
+    """
+    An exception is raised that will be exported by Lambda.
+
+    Args:
+        lineno : the line number of the code that failed
+        error  : the error description (where available)
+
+    Returns:
+        The Cloudwatch log stream URL containing the error
+    """
+
+    LOGGER.error('The following error has occurred on line: %s', lineno)
+    LOGGER.error(str(error))
+    sess = boto3.session.Session()
+    region = sess.region_name
+
+    raise Exception("https://{0}.console.aws.amazon.com/cloudwatch/home?region=\
+            {0}#logEventViewer:group={1};stream={2}".format\
+               (region, LOG_GROUP_NAME, LOG_STREAM_NAME))
 
 def send_message_to_slack(text):
     """
@@ -45,8 +67,8 @@ def send_message_to_slack(text):
             response = ssm.get_parameter(Name=ssm_param_name, WithDecryption=True)
         except ClientError as err:
             if err.response['Error']['Code'] == 'ParameterNotFound':
-                LOGGER.info('Slack SSM parameter %s not found. No notification sent', ssm_param_name)
-                return
+                LOGGER.info('Slack SSM parameter %s not found. No notification \
+                   sent', ssm_param_name)
             else:
                 LOGGER.error("Unexpected error when attempting to get Slack webhook URL: %s", err)
                 return
@@ -62,7 +84,8 @@ def send_message_to_slack(text):
             response = urllib.request.urlopen(req)
 
         else:
-            LOGGER.info('Value for Slack SSM parameter %s not found. No notification sent', ssm_param_name)
+            LOGGER.info('Value for Slack SSM parameter %s not found. No notification \
+               sent', ssm_param_name)
             return
 
     except Exception as err:
@@ -71,7 +94,17 @@ def send_message_to_slack(text):
             sys.exc_info()[2].tb_lineno)
         LOGGER.error(str(err))
 
-def lambda_handler(event , context):
+def lambda_handler(event, context):
+    """
+    Respond to triggering event by deleting unencrypted snapshots.
+
+    Args:
+        event (dict)           : Data about the triggering event
+        context (LamdaContext) : Runtime information
+
+    Returns:
+        null
+    """
     # Setup client
     ec2 = boto3.client('ec2')
 
@@ -80,27 +113,27 @@ def lambda_handler(event , context):
     snapshot_list = response['Snapshots']
 
     # Create list used by function
-    list = []
+    list_unencrypted = []
 
     # Iterate over the list of snapshots and store ones that are unencrypted
     for res in snapshot_list:
-        if res['Encrypted'] == False:
+        if res['Encrypted'] != 'Encrypted':
             print(res['SnapshotId'])
-            list.append(res)
+            list_unencrypted.append(res)
         else:
             continue
 
-        for res in list:
+        for resp in list_unencrypted:
             try:
                 # Try to delete the snapshot
-                ec2.delete_snapshot(SnapshotId=res['SnapshotId'])
+                ec2.delete_snapshot(SnapshotId=resp['SnapshotId'])
             except Exception as err:
                 error_handler(sys.exc_info()[2].tb_lineno, err)
                 # Search the error for the string below
-                if 'is currently in use by' in error:
+                if 'is currently in use by' in err:
                     try:
                         # Deregister the AMI
-                        ami = re.search("(ami-.*)", error)
+                        ami = re.search("(ami-.*)", err)
                         print("Deregistering AMI - " + ami.group(1))
                         ec2.deregister_image(ImageId=ami.group(1))
                     except Exception as err:
@@ -113,53 +146,5 @@ def lambda_handler(event , context):
                     except Exception as err:
                         error_handler(sys.exc_info()[2].tb_lineno, err)
                 else:
-                    print(res['SnapshotId'] + " - " + error)
-                continue
-
-
-def lambda_handler(event , context):
-    # Setup client
-    ec2 = boto3.client('ec2')
-
-    # Get snapshot list
-    response = ec2.describe_snapshots(OwnerIds=['self'])
-    snapshot_list = response['Snapshots']
-
-    # Create list used by function
-    list = []
-
-    # Iterate over the list of snapshots and store ones that are unencrypted
-    for res in snapshot_list:
-        if res['Encrypted'] == False:
-            print(res['SnapshotId'])
-            list.append(res)
-        else:
-            continue
-
-    # Present the list to the user, and require input to continue.
-
-        for res in list:
-            try:
-                # Try to delete the snapshot
-                ec2.delete_snapshot(SnapshotId=res['SnapshotId'])
-            except Exception as err:
-                error_handler(sys.exc_info()[2].tb_lineno, err)
-                # Search the error for the string below
-                if 'is currently in use by' in error:
-                    try:
-                        # Deregister the AMI
-                        ami = re.search("(ami-.*)", error)
-                        print("Deregistering AMI - " + ami.group(1))
-                        ec2.deregister_image(ImageId=ami.group(1))
-                    except Exception as err:
-                        error_handler(sys.exc_info()[2].tb_lineno, err)
-                    try:
-                        # Delete the snapshot
-                        print("Deleting snapshot...")
-                        ec2.delete_snapshot(SnapshotId=res['SnapshotId'])
-                        print(res['SnapshotId'] + " - " + "deleted.")
-                    except Exception as err:
-                        error_handler(sys.exc_info()[2].tb_lineno, err)
-                else:
-                    print(res['SnapshotId'] + " - " + error)
+                    print(res['SnapshotId'] + " - " + err)
                 continue
