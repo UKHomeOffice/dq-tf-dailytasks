4d41b0fa1202a2aa07a3dc7217668e1addc542ad
Jwanderema <wanderema@hotmail.co.uk>
2020-08-06 15:49:27 +0100

    turnoff shutdown

--- cloudwatch_events.tf
+++ cloudwatch_events.tf
@@ -36,7 +36,7 @@ resource "aws_cloudwatch_event_rule" "rds_shutdown" {
   name                = "daily_rds_shutdown"
   description         = "Shutdown RDS Instances in notprod evenings and weekend"
   schedule_expression = "cron(0 18 ? * MON-FRI *)"
-  is_enabled          = "true"
+  is_enabled          = "false"
 }
 
 resource "aws_cloudwatch_event_target" "rds_startup" {
@@ -59,7 +59,7 @@ resource "aws_cloudwatch_event_rule" "rds_startup" {
   name                = "daily_rds_startup"
   description         = "Startup RDS Instances in notprod mornings weekday"
   schedule_expression = "cron(00 6 ? * MON-FRI *)"
-  is_enabled          = "true"
+  is_enabled          = "false"
 }
 
 resource "aws_cloudwatch_event_target" "ec2_shutdown" {
@@ -80,7 +80,7 @@ resource "aws_cloudwatch_event_rule" "ec2_shutdown" {
   name                = "daily_ec2_shutdown"
   description         = "Shutdown EC2 Instances in notprod evenings and weekends"
   schedule_expression = "cron(0 18 ? * MON-FRI *)"
-  is_enabled          = "true"
+  is_enabled          = "false"
 }
 
 resource "aws_cloudwatch_event_target" "ec2_startup" {
@@ -101,5 +101,5 @@ resource "aws_cloudwatch_event_rule" "ec2_startup" {
   name                = "daily_ec2_startup"
   description         = "Startup EC2 Instances in notprod mornings weekday"
   schedule_expression = "cron(00 6 ? * MON-FRI *)"
-  is_enabled          = "true"
+  is_enabled          = "false"
 }
