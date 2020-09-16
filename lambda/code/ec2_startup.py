28c717cdcabfea5ed7ef95a9393dcb1f6dd10ac7
Haitham Abou Houly <haithamabouhouly@hodqadms-MacBook-Pro.local>
2020-09-16 10:15:48 +0100

    exclude stage, dev and deployment notprrod inst from start script

--- lambda/code/ec2_startup.py
+++ lambda/code/ec2_startup.py
@@ -3,12 +3,28 @@ import boto3
 # Defines current active region
 
 active_region = 'eu-west-2'
-
+inst_to_exclude=[]
 
 def lambda_handler(event, context):
 
     # Retrieve EC2 Instances
     notprod_instances = boto3.resource('ec2', region_name=active_region)
+
+    for instance in notprod_instances.instances.filter(
+    	Filters =[{'Name':'tag:Name',
+    			'Values': ['*stag*']}]):
+    	inst_to_exclude.append(instance)
+
+    for instance in notprod_instances.instances.filter(
+    	Filters =[{'Name':'tag:Name',
+    			'Values': ['*dev*']}]):
+    	inst_to_exclude.append(instance)
+
+    for instance in notprod_instances.instances.filter(
+    	Filters =[{'Name':'tag:Name',
+    			'Values': ['*deployment*']}]):
+    	inst_to_exclude.append(instance)
+
     for instance in notprod_instances.instances.all():
 
         print("Instance-ID: ", instance.id)
@@ -20,5 +36,6 @@ def lambda_handler(event, context):
 
         # Start the instances
         for instance in stopped_instances:
-            instance.start()
-            print('Started instance: ', instance.id)
+            if instance not in inst_to_exclude:
+                instance.start()
+                print('Started instance: ', instance.id)
