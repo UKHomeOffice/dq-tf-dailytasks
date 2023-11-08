c4cbc4baa389e69e7be68ec16afcbbc2049e5eda
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-11-08 10:52:49 +0000

    feat: yel-8578 added kms key dynamically

---
+++ kms.tf
@@ -0,0 +1,43 @@
+data "aws_caller_identity" "current" { 
+}
+
+resource "aws_kms_key" "dt_bucket_key" {
+  description             = "This key is used to encrypt daily tasks buckets"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Id": "daily-tasks-1",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": [
+                  "${data.aws_caller_identity.current.arn}",
+                  "${data.aws_caller_identity.current.account_id}"
+                ]
+            },
+            "Action": "kms:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow services use of the key",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "s3.amazonaws.com"
+            },
+            "Action": [
+                "kms:Encrypt",
+                "kms:GenerateDataKey*",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+}
