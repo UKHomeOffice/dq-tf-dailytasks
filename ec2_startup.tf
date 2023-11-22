a344db3df54b44d5a8c5b9a22f2c56e1e68f8836
Srikanth Arunachalam <116078089+s-arunachalam@users.noreply.github.com>
2023-11-22 12:14:51 +0000

    feat: yel-8578 modified policy

--- ec2_startup.tf
+++ ec2_startup.tf
@@ -114,11 +114,11 @@ resource "aws_iam_policy" "ec2_startup" {
         },
         {
             "Sid": "AllowKMSDecryption",
+            "Effect": "Allow", 
             "Action": [
                 "kms:Decrypt",
                 "kms:GenerateDataKey"
             ],
-            "Effect": "Allow",
             "Resource": "*"
         }
     ]
