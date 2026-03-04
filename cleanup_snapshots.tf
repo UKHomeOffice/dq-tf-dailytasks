6d93991f55f97bbdbf244a93cbbfba82a23c7099
Yousef Nasser <yousef.nasser@digital.homeoffice.gov.uk>
2026-03-04 13:33:44 +0000

    upgrade python version (#89)

--- cleanup_snapshots.tf
+++ cleanup_snapshots.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "cleanup_snapshots" {
   role             = aws_iam_role.cleanup_snapshots.arn
   handler          = "cleanup_snapshots.lambda_handler"
   source_code_hash = data.archive_file.cleanup_snapshots_zip.output_base64sha256
-  runtime          = "python3.7"
+  runtime          = "python3.11"
   timeout          = "900"
   memory_size      = "128"
 
